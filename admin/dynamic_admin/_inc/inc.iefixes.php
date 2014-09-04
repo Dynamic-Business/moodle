d5b8781308330f4027873eaec9dea5e93c7d541e
Paul McGovern <pdmc78@gmail.com>
2014-09-04 11:05:26 +0100

    SCORM icon replacement

---
+++ admin/dynamic_admin/_inc/inc.iefixes.php
@@ -0,0 +1,12 @@
+<!--[if IE]>
+	 <link href="../_css/iefixes.css" rel="stylesheet" type="text/css" />
+<![endif]--> 
+<!--[if IE 7]>
+	 <link href="../_css/ie7fixes.css" rel="stylesheet" type="text/css" />
+<![endif]--> 
+<!--[if IE 8]>
+	 <link href="../_css/ie8fixes.css" rel="stylesheet" type="text/css" />
+<![endif]--> 
+<!--[if IE 9]>
+	 <link href="../_css/ie9fixes.css" rel="stylesheet" type="text/css" />
+<![endif]--> 
