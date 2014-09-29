9ad8ff7e28a150d1685efed55296445ca5921d6c
Paul McGovern <pdmc78@gmail.com>
2014-09-29 11:46:37 +0100

    Further fixes from Issue log

--- admin/dynamic_admin/dynamic_enrolments/edit-group.php
+++ admin/dynamic_admin/dynamic_enrolments/edit-group.php
@@ -93,7 +93,7 @@ if ($row->datebefore == 0){
         // Date / Checkbox enable
         $(document).ready(function(){
 
-            $("#dateafter,#datebefore").datepicker({maxDate: 'Now',minDate: null});
+            $("#dateafter,#datebefore").datepicker({maxDate: 'Now',minDate: null,dateFormat: 'dd/mm/yy'});
             /*$("#dateafter,#datebefore").attr('disabled',true);
             $("#dateafter,#datebefore").attr('value', 'N/A');*/
 
