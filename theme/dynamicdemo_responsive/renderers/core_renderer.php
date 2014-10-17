52bc34f8bb303f7f150a79bf90afb269f27ed58d
callumlew <callum.statham@dynamicbusiness.co.uk>
2014-10-17 15:37:54 +0100

    ADD Header styles
    Split the nav from the header, added a min height to header. Have also
    included the brand colours to the variables.less files.

--- theme/dynamicdemo_responsive/renderers/core_renderer.php
+++ theme/dynamicdemo_responsive/renderers/core_renderer.php
@@ -20,6 +20,47 @@
 class theme_dynamicdemo_responsive_core_renderer extends theme_dynamicbase_core_renderer {
     protected $debugging = true;
 
+    public function dynamic_header($displayNav = true){
+        global $CFG, $SITE; ?>
 
+        <header role="banner" class="navbar moodle-has-zindex"><div class="container-fluid header">
+                    <div class="pull-right">
+                        <?php echo $this->login_info(); ?>
+                    </div>
+                    <a class="brand" href="<?php echo $CFG->wwwroot;?>"><h1><?php echo $SITE->shortname; ?></h1></a>
+                </div>
+            <nav role="navigation" class="navbar-inner-dyn">
+                
+         <?php  if($displayNav){
+                
+                    $this->navHTML();
+
+                } ?>
+            </nav>
+        </header>
+
+<?php }
+
+    public function navHTML(){
+        global $CFG; ?>
+        
+        <a class="menu-btn" data-toggle="collapse" data-target=".nav-collapse">
+            Menu
+        </a>
+        <div class="nav-wrap">
+            <ul class="nav main-nav">
+                <li><a href="<?php echo $CFG->wwwroot; ?>">Home</a></li>
+                <li><a href="<?php echo $CFG->wwwroot; ?>/course/">All Courses</a></li>
+                <li><a href="<?php echo $CFG->wwwroot; ?>/my/">My Courses</a></li>
+            </ul>
+        <?php if($this->is_administrator() || $this->is_groupmanager()){ ?>
+            <ul class="nav dynamic-reports">
+                <li><a href="<?php echo $CFG->wwwroot; ?>/admin/dynamic_admin/dynamic_enrolments">Group Administration</a></li>
+                <li><a href="<?php echo $CFG->wwwroot; ?>/admin/dynamic_admin/dynamic_reports">Group Reporting</a></li>
+            </ul>
+        <?php } ?>
+        </div>
+
+<?php }
 
 }
